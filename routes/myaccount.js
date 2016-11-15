8e4c1bc42fadf868d655fbcb5dd13dc1122e456b
Vincent Wang <v_wang@outlook.com>
2016-11-15 02:10:44 -0500

    Show Active listings account profiles for sellers

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -11,13 +11,15 @@ router.get('/', function(req, res, next) {
   connect.query('SELECT * FROM users WHERE userid = ' + req.cookies.userid + ' limit 1', function(err, rows, fields) {
     var user = rows[0];
     var soldItems = null;
+    var activeItems = null;
     if (req.cookies.isSupplier != null && req.cookies.isSupplier == 1) {
-      getSoldItems(req.cookies.userid, function(soldItems){
+      getItems(req.cookies.userid, function(soldItems, activeItems){
         res.render('myaccount',
           {
             user: user,
             loggedin: userLoggedIn,
             soldItems: soldItems,
+            activeItems: activeItems,
             isSupplier: req.cookies.isSupplier
           });
       });
@@ -41,16 +43,25 @@ router.post('/logout', function(req, res) {
   res.send('succuss');
 });
 
-function getSoldItems(supplier, callback) {
-  var selectQuery = "SELECT table1.*, table2.deliverytime FROM " +
+function getItems(supplier, callback) {
+  var selectSold  = "SELECT table1.*, table2.deliverytime FROM " +
                     "(SELECT t1.itemid, t1.brand, t1.model, t1.price, t2.price AS soldAt " +
                     "FROM Items AS t1 " +
                     "inner join VehicleTransaction AS t2 " +
                     "ON sold = 1 AND t1.itemid = t2.item AND t1.supplier = ?) AS table1 " +
                     "LEFT JOIN Deliveries AS table2 ON table1.itemid = table2.item";
-  var query = connect.query(selectQuery, [supplier], function(err, rows, fields) {
+  var selectActive =  "SELECT itemid, brand, model, price, biddable, minimumprice, currentbid, bidendtime " +
+                      "FROM BobaVehicles.Items " +
+                      "WHERE supplier = ? AND sold = 0";
+
+  var query = connect.query(selectSold, [supplier], function(err, rows, fields) {
     if (err) throw err;
-    callback(rows);
+    var soldItems = rows;
+    query = connect.query(selectActive, [supplier], function(err, rows, fields) {
+      var activeItems = rows;
+      callback(soldItems, activeItems);
+    })
+
   })
 }
 

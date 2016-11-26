92e0b40d0213b32852b47dc9d6abfefa98c6a027
Vincent Wang <v_wang@outlook.com>
2016-11-25 19:36:52 -0500

    Add searching feature

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,25 @@ router.get('/', function(req, res) {
 	});
 });
 
+router.post('/search', function(req,res){
+	var userLoggedIn = false;
+	userLoggedIn = (req.cookies.userid != null);
+
+	var pattern = '%'+req.body.searchkey+'%';
+
+	var searchQuery = 	"SELECT * FROM BobaVehicles.Items " +
+											"WHERE sold = 0 AND (brand LIKE ? or model LIKE ?)" +
+											"ORDER BY listedtime DESC";
+	var query = connect.query(searchQuery, [pattern, pattern], function(err, rows, fields) {
+			if (err) throw err;
+			res.render('index', {
+	  			items: rows,
+					loggedin: userLoggedIn,
+					isSupplier: req.cookies.isSupplier,
+	  	});
+	});
+});
+
 
 
 

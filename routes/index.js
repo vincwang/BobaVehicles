f4baeaa8f9d6ff42043189dbd893f52d31308121
Vincent Wang <v_wang@outlook.com>
2016-11-29 22:39:20 -0500

    Add pagination to show 18 vehicles

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,18 @@ connect.connect();
 router.get('/', function(req, res) {
 	var userLoggedIn = false;
 	userLoggedIn = (req.cookies.userid != null);
+	var currentPage = req.query.page;
+	if (currentPage == null) {
+		currentPage = 0;
+	}
 	connect.query('SELECT * FROM BobaVehicles.Items WHERE sold = 0 ORDER BY listedtime DESC', function(err, rows, fields) {
+		var totalPages = Math.ceil(rows.length / 18);
 		res.render('index', {
-  			items: rows,
+  			items: rows.slice(currentPage * 18, currentPage * 18 + 18),
 				loggedin: userLoggedIn,
 				isSupplier: req.cookies.isSupplier,
+				totalPages: totalPages,
+				currentPage: currentPage
   		});
 	});
 });
